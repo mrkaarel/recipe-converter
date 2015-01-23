9064c1247a7dfec93148b3208d1e96b9bc6f4cdd
Kaarel Julge <kaarel@gmail.com>
2015-01-23 10:48:44 +0200

    Added support for Fahrenheit to Celsius conversions

--- RecipeConverter.Website/Default.aspx.cs
+++ RecipeConverter.Website/Default.aspx.cs
@@ -18,7 +18,8 @@ namespace RecipeConverter
 			var ruleSet = new List<ConversionRule>();
 			ruleSet.Add(new ConversionRule() { SourceUnit = "pounds|pound|lbs|lb", TargetUnit = "kg", ConversionMethod = ConversionMethods.Factor(0.453592m) });
 			ruleSet.Add(new ConversionRule() { SourceUnit = "ounces|ounce|oz", TargetUnit = "g", ConversionMethod = ConversionMethods.Factor(28.3495m), TargetFormatString = "#" });
-			ruleSet.Add(new ConversionRule() { SourceUnit = "cups|cup", TargetUnit = "ml",  ConversionMethod = ConversionMethods.Factor(236.588m), TargetFormatString = "#" });
+			ruleSet.Add(new ConversionRule() { SourceUnit = "cups|cup", TargetUnit = "ml", ConversionMethod = ConversionMethods.Factor(236.588m), TargetFormatString = "#" });
+			ruleSet.Add(new ConversionRule() { SourceUnit = "deg F|degrees F|° F|°F", TargetUnit = "C", ConversionMethod = (x) => (x - 32) / 1.8m, TargetFormatString = "#" });
 
 			txtRecipe.Text = new Converter(ruleSet).Convert(input);
 		}
